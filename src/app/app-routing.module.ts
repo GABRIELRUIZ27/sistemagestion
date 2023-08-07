99e316fb838d7743fa4b7892fb7114aa0a6ad2c4
Gabriel <g27r02@gmail.com>
2023-08-07 12:17:14 -0600

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AdministradorComponent } from './administrador/administrador.component'; // Ajusta la ruta según tu estructura
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'administrador', component: AdministradorComponent }, // Nueva ruta para el componente "administrador"
+  // Otras rutas...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AdministradorComponent } from './administrador/administrador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    AdministradorComponent
   ],
   imports: [
     BrowserModule,
